e889d557c844e390ef3c5e71588c7ae2f107c4ad
Hanguyen <nguyenthuhatq1952004@gmail.com>
2025-04-28 14:37:05 +0000

    New update version 1

--- gemini.py
+++ gemini.py
@@ -1,18 +1,28 @@
+from google import genai
+from vertexai.generative_models import GenerationConfig
 
-import google.generativeai as genai
 
 class TeacherBot:
     def __init__(self, api_key: str):
-        genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel('gemini-1.5-pro')
-
+        # genai.configure(api_key=api_key)
+        # self.model = genai.GenerativeModel('gemini-1.5-pro')
+        self.client = genai.Client(api_key="AIzaSyCt4XAe9FaGohl1xVD6vgb6fZmbxo7_qwQ")
+        # response = client.models.generate_content(
+        #     model="gemini-2.0-flash",
+        #     contents="Explain how AI works in a few words",
+        # )
     def answer_question(self, question: str, language: str) -> str:
        
-        prompt = (
-            f"Bạn hãy đóng vai giáo viên chuyên nghiệp, trả lời ngắn gọn bằng ngôn ngữ '{language}'. "
+        context = (
+            f"Bạn hãy đóng vai giáo viên chuyên nghiệp, trả lời ngắn gọn nhất có thể bằng ngôn ngữ '{language}'. "
             f"Dưới đây là câu hỏi:\n\n{question}"
         )
-        response = self.model.generate_content(prompt)
+  
+        response = self.client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=context,
+
+        )
         return response.text.strip()
 
 if __name__ == "__main__":
@@ -26,3 +36,5 @@ if __name__ == "__main__":
     print("Câu trả lời:", answer)
 
     
+
+
